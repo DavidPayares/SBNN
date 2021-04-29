b1e40087dcc0709877077d454ac4d4d1d2f5e6d9
DavidPayares <43214524+DavidPayares@users.noreply.github.com>
2021-04-29 18:11:08 +0200

    Update SBNN_dataset.py

---
+++ models/SBNN_dataset.py
@@ -0,0 +1,9 @@
+# Neurodegenerative Diseases Classification
+# Load Data for SBNN model.
+# Author: David Payares.
+# Copyleft: MIT Licience.
+
+import numpy as np
+import zipfile
+
+class SBNNDataset(object)
